48430d4015ca50ad5e98d5d16d081310879ee2a1
Student <asadullah3453rajput@gmail.com>
2022-12-23 08:00:42 +0500

    pd1

---
+++ dog.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << " -----------------------------------------------        " <<endl;
+cout << "    O      ^__^                                         " <<endl;
+cout << "       O   (oo)\__________                              " <<endl;
+cout << "           (__)\          )\/\                          " <<endl;
+cout << "               ||-------W |                             " <<endl;
+cout << "               ||        ||                             " <<endl;
+cout << "               **        **                             " <<endl;
+}
